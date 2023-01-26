4eb876b8f0ca8d4bca64631c409547e44c01f3c8
odiljonov <odiljonovbe@gmail.com>
2023-01-26 07:58:07 +0500

    My React Movie App finished
    This project is very useful and interesting

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Header from "./layout/Header";
+import Main from "./layout/Main";
+import Footer from "./layout/Footer";
+import React from "react";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
+
+
+
+
+
+
---
+++ src/layout/Footer.js
@@ -0,0 +1,13 @@
+export  default function Footer() {
+    return (
+
+        <footer className="page-footer #80deea cyan lighten-3">
+            <div className="footer-copyright #80deea cyan lighten-3">
+                <div className="container">
+                    © {new Date().getFullYear()} Copyright Odiljonov O'tkirbek
+                    <a className="grey-text text-lighten-4 right" href="#!">Movie</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
